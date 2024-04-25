bc1842c750f6bdcc3de33cfdec79a0456ddbf55a
Saumya <surasaumya17@gmail.com>
2024-04-25 17:33:36 +0530

    only validaotr remaining

--- lib/login.dart
+++ lib/login.dart
@@ -9,6 +9,7 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.blue,
--- lib/todo.dart
+++ lib/todo.dart
@@ -1,51 +1,59 @@
-
-// ignore_for_file: prefer_const_constructors, camel_case_types
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
 
 class TODO extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
+      
       home: const MYHOME(),
-
     );
-
-   
   }
 }
 
-  class MYHOME extends StatefulWidget {
-  const MYHOME({super.key});
+class MYHOME extends StatefulWidget {
+  const MYHOME({Key? key}) : super(key: key);
 
   @override
   State<MYHOME> createState() => _MYHOMEState();
 }
 
 class _MYHOMEState extends State<MYHOME> {
-  
+  List<String> todoo = [];
+  String newtodo = ""; // Define newtodo here
 
-  void todo()
-  {
-    setState(() {
-       
-       showDialog(context: context, builder: (BuildContext){
-        String newtodo="";
+  void todo() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
         return AlertDialog(
           title: Text("TODO"),
           content: TextField(
-            onChanged: (value){
-              newtodo=value;
+            onChanged: (value) {
+              setState(() {
+                newtodo = value; // Update newtodo here
+              });
             },
           ),
+          actions: [
+            ElevatedButton(onPressed: (){
+              setState(() {
+                todoo.add(newtodo); // Add newtodo to todoo here
+              });
+            }, child: Text("ADD"))
+          ],
         );
-       });
-
-    });
+      },
+    );
   }
+
+  
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: Text('TODO Page'),
         actions: [
@@ -55,12 +63,33 @@ class _MYHOMEState extends State<MYHOME> {
           ),
         ],
       ),
-      
+      body: ListView.builder(
+        itemCount: todoo.length,
+        itemBuilder: (context, index) {
+          return Card(
+
+            child: ListTile(
+              title: Text(
+                todoo[index],
+                style: TextStyle(fontSize: 18),
+              ),
+              trailing: IconButton(
+                icon: Icon(Icons.delete),
+                onPressed: () {
+                  setState(() {
+                    todoo.removeAt(index);
+                  });
+                },
+              ),
+            ),
+          );
+        },
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: todo,
-        tooltip: 'Increment',
+        onPressed: todo, 
+        tooltip: 'Add Todo',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
